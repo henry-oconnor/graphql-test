64bd641377851c34a644e6663d0dac064c7511a4
Henry O'Connor <hen.oconnor@gmail.com>
2019-10-01 13:35:59 -0400

    work on tutorial

---
+++ src/app/exchange-rates/exchange-rates.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+class Response {
+  rates: any[] = [];
+  loading: boolean = true;
+  error: any = "";
+}
+
+@Component({
+  selector: 'app-exchange-rates',
+  templateUrl: './exchange-rates.component.html',
+  styleUrls: ['./exchange-rates.component.css']
+})
+
+export class ExchangeRatesComponent implements OnInit {
+  public response: Response;
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit() {
+    console.log('here');
+    this.apollo
+    .watchQuery<Response>({
+      query: gql`
+      {
+        rates(currency: "$currency") {
+          currency
+          rate
+        }
+      }
+      `,
+      variables: { currency: 'USD'}
+    })
+    .valueChanges.subscribe(result => {
+      this.response.rates = result.data && result.data.rates;
+      this.response.loading = result.loading;
+      this.response.error = result.errors;
+    });
+  }
+
+}
--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
-const uri = ''; // <-- add the URL of the GraphQL server here
+const uri = 'https://o5x5jzoo7z.sse.codesandbox.io/graphql'; // our test Graphql Server which returns rates
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({uri}),
